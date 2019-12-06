a7d8bde4da5c59df1758272a95a3fde9e8a2db61
choward174 <56250704+choward174@users.noreply.github.com>
2019-12-06 18:52:46 +0000

    Stable build 06/12/2019

---
+++ New Unity Project (1)/Assets/Scripts/HunterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+
+        
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/PropMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    public Transform lookAt;
+
+    public Camera cam;
+
+    public bool isGrounded;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        cam.enabled = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+
+
+    }
+        void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.right * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 2, 0) * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+
+        void LateUpdate()
+    {
+       transform.LookAt(new Vector3(lookAt.position.x, transform.position.y, lookAt.position.z));
+    }
+}
